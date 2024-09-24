15e1aa485a34e796b73a81dd5428f112c1839699
surayaahlam <surayaahlam@gmail.com>
2024-09-25 01:55:03 +0600

    github blog.html link added

--- JS/switch.js
+++ JS/switch.js
@@ -1,9 +1,9 @@
 // To Blog page
 document.getElementById("blog-btn").addEventListener('click', function(){
-    window.location.href = "/blog.html";
+    window.location.href = "https://surayaahlam.github.io/DOM-Quest-Build-and-Manipulate-assignment-5-ph/blog.html";
 });
 
 // To Home Page
 // document.getElementById("home-btn").addEventListener('click', function(){
-//     window.location.href = "/index.html";
+//     window.location.href = "https://surayaahlam.github.io/DOM-Quest-Build-and-Manipulate-assignment-5-ph/";
 // })
